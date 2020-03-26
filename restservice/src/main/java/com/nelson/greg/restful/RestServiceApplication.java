efff415e9d90e1f30dc35215321d85a42404b55d
Greg Nelson <nelsongreg35@gmail.com>
2020-03-25 20:16:06 -0500

    Back to spring boot and not a servlet

--- restservice/src/main/java/com/nelson/greg/restful/RestServiceApplication.java
+++ restservice/src/main/java/com/nelson/greg/restful/RestServiceApplication.java
@@ -2,9 +2,11 @@ package com.nelson.greg.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class RestServiceApplication {
+//	public class RestServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
