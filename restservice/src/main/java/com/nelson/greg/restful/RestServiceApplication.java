169b05166c497234c14189be3792ebd3b46a6fbc
Greg Nelson <nelsongreg35@gmail.com>
2020-04-26 10:57:03 -0500

    Added Actuator and HTTP Tracing

--- restservice/src/main/java/com/nelson/greg/restful/RestServiceApplication.java
+++ restservice/src/main/java/com/nelson/greg/restful/RestServiceApplication.java
@@ -1,12 +1,27 @@
 package com.nelson.greg.restful;
 
 import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+//import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class RestServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
+	
+//	@Configuration
+//	// @Profile("actuator-endpoints") /* if you want: register bean only if profile is set */
+//	public class HttpTraceActuatorConfiguration {
+//
+//	    @Bean
+//	    public HttpTraceRepository httpTraceRepository() {
+//	        return new InMemoryHttpTraceRepository();
+//	    }
+//
+//	}
 
 }
