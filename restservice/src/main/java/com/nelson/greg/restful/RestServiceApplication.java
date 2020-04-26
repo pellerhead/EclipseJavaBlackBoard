166072f13fb3b13954a441a2c17096db3b8768d4
Greg Nelson <nelsongreg35@gmail.com>
2020-04-25 19:25:39 -0500

    Adding SWAGGER2

--- restservice/src/main/java/com/nelson/greg/restful/RestServiceApplication.java
+++ restservice/src/main/java/com/nelson/greg/restful/RestServiceApplication.java
@@ -2,12 +2,9 @@ package com.nelson.greg.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class RestServiceApplication {
-//	public class RestServiceApplication extends SpringBootServletInitializer {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
